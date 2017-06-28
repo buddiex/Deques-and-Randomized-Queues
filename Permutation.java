4f0d6365b4b8c57e6f95bdcdc0381414173b8a39
osayamen <mailbuddie@gmail.com>
2017-06-28 12:15:49 +0100

    complete permutation.java

--- Permutation.java
+++ Permutation.java
@@ -1,6 +1,10 @@
 public class Permutation {
-	public static void main(String[] args) {
-		StdOut.println("sdfd");
-		StdOut.println(args[0]);
-	}
+    public static void main(String[] args) {
+        int k = Integer.parseInt(args[0]);
+        String[] strs = StdIn.readAllStrings();
+        StdRandom.shuffle(strs);
+        for (int i = 0; i < k; i++) {
+            StdOut.println(strs[i]);
+        }
+    }
 }
